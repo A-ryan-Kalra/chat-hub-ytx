e1b034cd4fd0c92ab7e4b784123c573d87d3806c
Aryan Kalra <aryan.smart832@gmail.com>
2025-01-25 15:23:30 +0530

    settin up shadcn

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,13 @@
+"use client";
+
+import { ModeToggle } from "@/components/model-toggle";
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <div className="">This is a protected route</div>;
+  return (
+    <div className="">
+      <UserButton />
+      <ModeToggle />
+    </div>
+  );
 }
