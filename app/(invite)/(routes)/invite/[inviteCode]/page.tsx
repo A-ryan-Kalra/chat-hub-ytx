4dcd024b62b6a99784aa8592183aed0ad32799d9
Aryan Kalra <aryan.smart832@gmail.com>
2025-02-16 17:44:37 +0530

    fix invitation code

--- app/(invite)/(routes)/invite/[inviteCode]/page.tsx
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -1,11 +1,67 @@
-import React from "react";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { RedirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 interface InviteCodeProps {
   params: { inviteCode: string };
 }
 
 async function InviteCode({ params }: InviteCodeProps) {
-  return <div>InviteCode</div>;
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return RedirectToSignIn({});
+  }
+
+  if (!params.inviteCode) {
+    return redirect("/");
+  }
+
+  const invalidInviteCode = await db.server.findFirst({
+    where: {
+      inviteCode: params.inviteCode,
+    },
+  });
+
+  if (!invalidInviteCode) {
+    return <div className="">Invite code is invalid</div>;
+  }
+
+  const existingServer = await db.server.findFirst({
+    where: {
+      inviteCode: params.inviteCode,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (existingServer) {
+    return redirect(`/servers/${existingServer.id}`);
+  }
+
+  const server = await db.server.update({
+    where: {
+      inviteCode: params.inviteCode,
+    },
+    data: {
+      members: {
+        create: [
+          {
+            profileId: profile.id,
+          },
+        ],
+      },
+    },
+  });
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return null;
 }
 
 export default InviteCode;
