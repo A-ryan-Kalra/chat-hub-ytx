801be53606a99e096fd6a5f0b98780edfedfe888
Aryan Kalra <aryan.smart832@gmail.com>
2025-02-16 10:47:41 +0530

    done with sidebar configuration for channel

--- components/sever/server-header.tsx
+++ components/sever/server-header.tsx
@@ -6,12 +6,15 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import {
   ChevronDown,
+  LogOut,
   PlusCircle,
   Settings,
+  Trash,
   UserPlus,
   Users,
 } from "lucide-react";
@@ -56,6 +59,19 @@ function ServerHeader({ role, server }: ServerHeaderProps) {
           <DropdownMenuItem className=" px-3 py-2 textsm cursor-pointer">
             Create Channel
             <PlusCircle className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {isModerator && <DropdownMenuSeparator />}
+        {isAdmin && (
+          <DropdownMenuItem className="text-rose-500 px-3 py-2 textsm cursor-pointer">
+            Delete Server
+            <Trash className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {!isAdmin && (
+          <DropdownMenuItem className="text-rose-500 px-3 py-2 textsm cursor-pointer">
+            Leave Server
+            <LogOut className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
         )}
       </DropdownMenuContent>
