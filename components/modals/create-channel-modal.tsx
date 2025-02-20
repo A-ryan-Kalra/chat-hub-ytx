27f8ef3e799263c1ff238032dc3958f431f05e97
Aryan Kalra <aryan.smart832@gmail.com>
2025-02-20 21:46:34 +0530

    vhange background color of navigation sidebar

--- components/modals/create-channel-modal.tsx
+++ components/modals/create-channel-modal.tsx
@@ -34,6 +34,7 @@ import {
   SelectValue,
 } from "../ui/select";
 import queryString from "query-string";
+import { useEffect } from "react";
 
 const formSchema = z.object({
   name: z
@@ -48,19 +49,27 @@ const formSchema = z.object({
 });
 
 export const CreateChanelModal = () => {
-  const { isOpen, onClose, onOpen, type } = useModal();
+  const { isOpen, onClose, onOpen, type, data } = useModal();
   const router = useRouter();
   const params = useParams();
-
+  const { channelType } = data;
   const isModalOpen = isOpen && type === "createChannel";
 
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
-      type: ChannelType.TEXT,
+      type: channelType || ChannelType.TEXT,
     },
   });
+
+  useEffect(() => {
+    if (channelType) {
+      form.setValue("type", channelType);
+    } else {
+      form.setValue("type", ChannelType.TEXT);
+    }
+  }, [channelType, form]);
 
   const isLoading = form.formState.isSubmitting;
 
